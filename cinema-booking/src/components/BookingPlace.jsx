3609ec4ec26a519114aca3bd506422fbd48da189
Danil-Shevchenko <danilsheva8@gmail.com>
2025-07-03 19:27:20 +0300

    03.07 - added react router, update layout

--- cinema-booking/src/components/BookingPlace.jsx
+++ cinema-booking/src/components/BookingPlace.jsx
@@ -5,19 +5,6 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
-const seatsData = [
-  { row: "A", number: 1, status: "free" },
-  { row: "A", number: 2, status: "booked" },
-  { row: "A", number: 3, status: "free" },
-  { row: "A", number: 4, status: "booked" },
-  { row: "A", number: 5, status: "free" },
-  { row: "B", number: 1, status: "booked" },
-  { row: "B", number: 2, status: "free" },
-  { row: "B", number: 3, status: "booked" },
-  { row: "B", number: 4, status: "booked" },
-  { row: "B", number: 5, status: "booked" },
-];
-
 const groupByRow = (data) => {
   return data.reduce((acc, seat) => {
     acc[seat.row] = acc[seat.row] || [];
@@ -42,7 +29,7 @@ const Seat = styled(Paper, {
   },
 }));
 
-export default function BookingPlace() {
+export default function BookingPlace({ movie }) {
   const [selectedSeats, setSelectedSeats] = React.useState([]);
 
   const toggleSeat = (seat) => {
@@ -54,82 +41,74 @@ export default function BookingPlace() {
     );
   };
 
-  const seatsByRow = groupByRow(seatsData);
+  const seatsByRow = groupByRow(movie.seats);
 
   return (
     <Box
       sx={{
+        position: "relative",
         width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        placeItems: "center",
+        maxWidth: "900px",
+        borderRadius: "8px",
+        overflow: "hidden",
       }}
     >
       <Box
         sx={{
-          position: "relative",
-          width: 600,
-          overflow: "hidden",
-        }}
-      >
-        <Box
-          sx={{
+          position: "absolute",
+          inset: 0,
+          backgroundImage: `url(${movie.area[0].background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          zIndex: 0,
+          "&::before": {
+            content: '""',
             position: "absolute",
             inset: 0,
-            backgroundImage:
-              'url("https://catchfoundation.in/_next/image?url=https%3A%2F%2Fcatch-foundation-s3-bucket-production.s3.ap-south-1.amazonaws.com%2FWildlife_informer_6a7110457f.webp&w=3840&q=75")',
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            zIndex: 0,
-            "&::before": {
-              content: '""',
-              position: "absolute",
-              inset: 0,
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-            },
-          }}
-        />
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+        }}
+      />
 
-        <Box
-          sx={{
-            position: "relative",
-            zIndex: 1,
-            display: "flex",
-            flexDirection: "column",
-            placeItems: "center",
-            px: 2,
-          }}
-        >
-          <Box sx={{ width: "100%", maxWidth: 200 }}>
-            {Object.entries(seatsByRow).map(([rowName, rowSeats]) => (
-              <Box key={rowName} sx={{ mb: 2 }}>
-                <Typography
-                  variant="h6"
-                  sx={{ mb: 1, color: "white", textAlign: "center" }}
-                >
-                  Row {rowName}
-                </Typography>
-                <Grid container spacing={2} justifyContent="center">
-                  {rowSeats.map((seat) => {
-                    const seatId = `${seat.row}${seat.number}`;
-                    const isSelected = selectedSeats.includes(seatId);
-                    return (
-                      <Grid item key={seatId}>
-                        <Seat
-                          onClick={() => toggleSeat(seat)}
-                          booked={seat.status === "booked"}
-                          selected={isSelected}
-                          elevation={3}
-                        >
-                          {seatId}
-                        </Seat>
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </Box>
-            ))}
-          </Box>
+      <Box
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          display: "flex",
+          flexDirection: "column",
+          placeItems: "center",
+          px: 2,
+        }}
+      >
+        <Box sx={{ width: "100%", maxWidth: 200 }}>
+          {Object.entries(seatsByRow).map(([rowName, rowSeats]) => (
+            <Box key={rowName} sx={{ mb: 2 }}>
+              <Typography
+                variant="h6"
+                sx={{ mb: 1, color: "white", textAlign: "center" }}
+              >
+                Row {rowName}
+              </Typography>
+              <Grid container spacing={2} justifyContent="center">
+                {rowSeats.map((seat) => {
+                  const seatId = `${seat.row}${seat.number}`;
+                  const isSelected = selectedSeats.includes(seatId);
+                  return (
+                    <Grid item key={seatId}>
+                      <Seat
+                        onClick={() => toggleSeat(seat)}
+                        booked={seat.status === "booked"}
+                        selected={isSelected}
+                        elevation={3}
+                      >
+                        {seatId}
+                      </Seat>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Box>
+          ))}
         </Box>
       </Box>
     </Box>
--- cinema-booking/src/components/Header.jsx
+++ cinema-booking/src/components/Header.jsx
@@ -1,61 +1,69 @@
-import logo from '../assets/logo.png'
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
+import { useLocation, useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
 }));
 
-export default function Header({search, setSearch}) {
-    const handleChange = (e) =>{
-    setSearch(e.target.value)
-  }
+export default function Header({ search, setSearch }) {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+   const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/`);
+  };
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: '#2e3b55' }}>
+    <Box sx={{ flexGrow: 1, mb:2 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#2e3b55" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -63,27 +71,31 @@ export default function Header({search, setSearch}) {
             color="inherit"
             aria-label="open drawer"
             sx={{ mr: 2 }}
+            onClick={() => handleClick()}
           >
-          <img src={logo} alt="logo" width={"50px"}/>
+            <img src={logo} alt="logo" width={"50px"} />
           </IconButton>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             CinemaBT
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              value={search} onChange={handleChange}
-            />
-          </Search>
+          {isHomePage && (
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+                value={search}
+                onChange={handleChange}
+              />
+            </Search>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
